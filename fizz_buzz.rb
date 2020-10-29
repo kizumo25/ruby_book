a7063eedb602411ad79fbfd0cf59aec85f7fb85a
kizumo25 <k.patagonic5@gmail.com>
2020-10-29 10:55:38 +0900

    update and minitest added fizz_buzz.rb

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,19 +1,25 @@
 def fizz_buzz(x)
-	if x % 3 == 0
+	if x % 15 == 0
+		"Fizz Buzz"
+	elsif x % 3 == 0
 		"Fizz"
 	elsif x % 5 == 0
 		"Buzz"
-	elsif x % 15 == 0
-		"Fizz Buzz"
 	else
 		x.to_s
 	end
 end
 
-puts fizz_buzz(1)
-puts fizz_buzz(2)
-puts fizz_buzz(3)
-puts fizz_buzz(4)
-puts fizz_buzz(5)
-puts fizz_buzz(6)
-puts fizz_buzz(15)
+require 'minitest/autorun'
+
+class Fizz_BuzzTest < Minitest::Test
+	def test_fizz_buzz
+		assert_equal '1', fizz_buzz(1)
+		assert_equal '2', fizz_buzz(2)
+		assert_equal 'Fizz', fizz_buzz(3)
+		assert_equal '4', fizz_buzz(4)
+		assert_equal 'Buzz', fizz_buzz(5)
+		assert_equal 'Fizz', fizz_buzz(6)
+		assert_equal 'Fizz Buzz', fizz_buzz(15)
+	end
+end
