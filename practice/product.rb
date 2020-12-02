9c7cfaf421080237a7b3f0e018b2b86235b913bd
kizumo25 <k.patagonic5@gmail.com>
2020-12-02 16:21:50 +0900

    add product.rb product_over_ride.rb

---
+++ practice/product.rb
@@ -0,0 +1,26 @@
+class  Product
+	attr_accessor :name, :price
+
+	def initialize(name, price)
+		@name = name
+		@price = price
+	end
+end
+product = Product.new('A great movie', 1000)
+# p product.name
+# p product.price
+
+class DVD < Product
+	attr_reader :running_time
+
+	def initialize(name, price, running_time)
+		super(name, price)
+		@running_time = running_time
+	end
+end
+dvd = DVD.new('A great movie', 1000, 120)
+p dvd.name
+p dvd.price
+p dvd.running_time
+
+
